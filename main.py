40edcb09475091c32750d437da1c9b192eb2b0f8
gallardo0825 <93864563+gallardo0825@users.noreply.github.com>
2024-06-22 23:38:03 +0900

    KF動作確認

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from scipy.linalg import inv, sqrtm
+from numpy import identity as eye
+import matplotlib.pyplot as plt
+import random
+from Calculation import Calculation
+from KF import KF
+
+cal = Calculation()
+kf = KF(calculation=cal)
+kf_a, kf_f = kf.run_simulation()
+ls = range(len(kf_a))
+plt.figure(figsize=(10, 6))
+plt.xlabel('Time step')
+plt.ylabel('RMS of absolute value of epsilon')
+plt.plot(ls, kf_a, label="KF")
+plt.show()
